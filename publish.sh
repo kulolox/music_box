00474e1acae003d6dec2781c02a47feb9ac277d1
kulolox <1048016718@qq.com>
2019-11-21 10:55:50 +0800

    切换api接口

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 npm run build
 chmod +x ./scripts/publish-pages.sh
-./scripts/publish-pages.sh -d ../music.kulolox.cn -g git@e.coding.net:button-dev/pages/jpm2020.button.tech.git
-curl https://aliyun.kulolox.cn:3020/music.kulolox.cn
+./scripts/publish-pages.sh -d ../music.kulolox.cn -g git@git.dev.tencent.com:kulolox/music.kulolox.cn.git
+curl http://aliyun.kulolox.cn:3020/music.kulolox.cn
