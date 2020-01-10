167a653fa6cbc5f115f82ad386f9790ba1918038
kulolox <1048016718@qq.com>
2020-01-10 15:05:26 +0800

    delete publish.sh

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-npm run build
-chmod +x ./scripts/publish-pages.sh
-./scripts/publish-pages.sh -d ../music.kulolox.cn -g git@git.dev.tencent.com:kulolox/music.kulolox.cn.git
-curl http://aliyun.kulolox.cn:3020/music.kulolox.cn
